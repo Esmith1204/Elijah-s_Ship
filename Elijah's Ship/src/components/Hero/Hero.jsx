9872e2d59eb94885521e19b91b858d9317fd9161
Elijah <elijahcs@uci.edu>
2025-05-04 06:09:26 -0700

    Added hero animation

--- Elijah's Ship/src/components/Hero/Hero.jsx
+++ Elijah's Ship/src/components/Hero/Hero.jsx
@@ -1,11 +1,43 @@
-import React from 'react';
+import React, {useRef, useLayoutEffect} from 'react';
+import { gsap } from 'gsap';
+import { SplitText } from 'gsap/SplitText';
+
+gsap.registerPlugin(SplitText);
 
 export default function Hero() {
+
+    const titleRef = useRef(null)
+    const descRef = useRef(null)
+    const btnRef = useRef(null)
+
+    useLayoutEffect(() => {
+        const split = SplitText.create(titleRef.current, {
+            type: 'chars,words,lines'
+          })
+
+        const tl = gsap.timeline()
+
+        tl.from(split.chars, {
+            x: 150,
+            opacity: 0,
+            duration: 0.7,
+            ease: 'power4.out',
+            stagger: 0.04
+          })
+
+          return () => {
+            split.revert()
+            tl.kill()
+          }
+        }, [])
+      
     return (
         <div className="hero bg-base-200 min-h-screen">
             <div className="hero-content text-center">
                 <div className="max-w-md">
-                <h1 className="text-5xl font-bold">Elijah's Ship</h1>
+                <h1 ref={titleRef} className="text-5xl font-bold">
+                    Elijah's Ship
+                </h1>
                 <p className="py-6">
                     AKA: My website portfolio :]<br />
                 </p>
